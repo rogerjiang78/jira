cdfa75fb30147869c61ffd84c5e7cbd986a53c35
Roger_JIANG <rogerjiang78@gmail.com>
2021-07-14 14:48:18 +1000

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { loadDevTools } from "jira-dev-tool";
+import { loadServer } from "jira-dev-tool";
 import "antd/dist/antd.less";
 import { AppProvider } from "context";
 
-loadDevTools(() =>
+loadServer(() =>
   ReactDOM.render(
     <React.StrictMode>
       <AppProvider>
